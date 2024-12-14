ed3750b28cc9db885e214de60a5284f43c46c628
hieunm_U2U <hieunm@uniultra.xyz>
2024-12-14 19:13:10 +0700

    [Update] List Pool pUSDT

--- src/templateEnum.ts
+++ src/templateEnum.ts
@@ -1,6 +1,6 @@
 export namespace ContractAddress {
     export const ZERO = "0x0000000000000000000000000000000000000000"
-    export const listPool = ["{{pool1.address}}","{{pool2.address}}"]
+    export const listPool = ["{{pool1.address}}"]
     export const publicPoolAddress = ["{{incentivepoolpublic.address}}","{{incentivepoolpublic2.address}}"]
     export const bitgetPoolAddress = ["{{incentivepoolbitget.address}}"]
 
