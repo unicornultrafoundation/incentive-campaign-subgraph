c3265602ce068934b2be2d0c3338055b64fd6cd7
hieunm_U2U <hieunm@uniultra.xyz>
2024-12-14 16:37:36 +0700

    [Update] Change Block testnet

--- src/mapping/IncentivePool.ts
+++ src/mapping/IncentivePool.ts
@@ -1,5 +1,5 @@
 import { Stake,UnStake,Harvest} from '../../generated/IncentivePoolPublic/IncentivePool';
-import { LegacyUnStake } from '../../generated/IncentivePoolPublic2/IncentivePool2';
+// import { LegacyUnStake } from '../../generated/IncentivePoolPublic2/IncentivePool2';
 
 import { Address, BigInt, log } from "@graphprotocol/graph-ts"
 import {TransactionPool} from '../../generated/schema';
@@ -7,6 +7,7 @@ import {generateCombineKey, updateStatisticsDashboard, updateStatisticsUser, isA
 import { ContractAddress, DashBoardStatistics, UserStatistics } from '../enum';
 
 export function handleStake(event: Stake): void{
+    log.info("================================ {}", [event.address.toHexString()]);
     let id = generateCombineKey([event.transaction.hash.toHexString(),'STAKE'])
     let tx = TransactionPool.load(id);
     if(!tx) {
@@ -149,50 +150,50 @@ export function handleHarvest(event: Harvest) : void{
     }
 }
 
-export function handlerLegacyUnStake(event: LegacyUnStake) : void {
-    let id = generateCombineKey([event.transaction.hash.toHexString(),'UNSTAKELEGACY'])
-    let tx = TransactionPool.load(id);
-    if(!tx) {
-        tx = new TransactionPool(id);
-        tx.contract = event.address.toHexString();
-        tx.from = event.address.toHexString();
-        tx.to = event.params.user.toHexString();
-        tx.txHash = event.transaction.hash.toHexString();
-        tx.event = 'UNSTAKELEGACY';
-        tx.amount = event.params.amount;
-        tx.timestamp = event.block.timestamp;
-    }
-    tx.save();
-    // ==========User =============
-    updateStatisticsUser(event.params.user,UserStatistics.amount,event.params.amount.neg(), event.address);
-    updateStatisticsUser(event.params.user,UserStatistics.amountUnStake,event.params.amount, event.address);
-    updateStatisticsUser(event.params.user,UserStatistics.totalUnStake,BigInt.fromI32(1), event.address);
-    updateStatisticsUser(event.params.user,UserStatistics.totalTransaction,BigInt.fromI32(1), event.address);
-    // ==========Dashboard =============
-    updateStatisticsDashboard(DashBoardStatistics.totalTransaction,BigInt.fromI32(1));
-    updateStatisticsDashboard(DashBoardStatistics.amountUnStake,event.params.amount);
-    updateStatisticsDashboard(DashBoardStatistics.totalUnStake,BigInt.fromI32(1));
-    if(isAddressInArray(event.address.toHexString().toLowerCase(), ContractAddress.publicPoolAddress)){
-        // ==========User Public=============
-        updateStatisticsUser(event.params.user,UserStatistics.amountPublic,event.params.amount.neg(), event.address);
-        updateStatisticsUser(event.params.user,UserStatistics.amountUnStakePublic,event.params.amount, event.address);
-        updateStatisticsUser(event.params.user,UserStatistics.totalUnStakePublic,BigInt.fromI32(1), event.address);
-        updateStatisticsUser(event.params.user,UserStatistics.totalTransactionPublic,BigInt.fromI32(1), event.address);
-        // ==========Dashboard Public=============
-        updateStatisticsDashboard(DashBoardStatistics.totalTxPublic,BigInt.fromI32(1));
-        updateStatisticsDashboard(DashBoardStatistics.amountUnStakePublic, event.params.amount)
-        updateStatisticsDashboard(DashBoardStatistics.totalUnStakePublic,BigInt.fromI32(1));
-    }
-    // if(event.address.toHexString().toLowerCase() == ContractAddress.bitgetPoolAddress.toLowerCase()){
-    if(isAddressInArray(event.address.toHexString().toLowerCase(), ContractAddress.bitgetPoolAddress)){
-        // ==========User Bitget=============
-        updateStatisticsUser(event.params.user,UserStatistics.amountBitget,event.params.amount.neg(), event.address);
-        updateStatisticsUser(event.params.user,UserStatistics.amountUnStakeBitget,event.params.amount, event.address);
-        updateStatisticsUser(event.params.user,UserStatistics.totalUnStakeBitget,BigInt.fromI32(1), event.address);
-        updateStatisticsUser(event.params.user,UserStatistics.totalTransactionBitget,BigInt.fromI32(1), event.address);
-        // ==========Dashboard Bitget=============
-        updateStatisticsDashboard(DashBoardStatistics.totalTxBitget,BigInt.fromI32(1));
-        updateStatisticsDashboard(DashBoardStatistics.amountUnStakeBitget, event.params.amount)
-        updateStatisticsDashboard(DashBoardStatistics.totalUnStakeBitget,BigInt.fromI32(1));
-    }
-}
+// export function handlerLegacyUnStake(event: LegacyUnStake) : void {
+//     let id = generateCombineKey([event.transaction.hash.toHexString(),'UNSTAKELEGACY'])
+//     let tx = TransactionPool.load(id);
+//     if(!tx) {
+//         tx = new TransactionPool(id);
+//         tx.contract = event.address.toHexString();
+//         tx.from = event.address.toHexString();
+//         tx.to = event.params.user.toHexString();
+//         tx.txHash = event.transaction.hash.toHexString();
+//         tx.event = 'UNSTAKELEGACY';
+//         tx.amount = event.params.amount;
+//         tx.timestamp = event.block.timestamp;
+//     }
+//     tx.save();
+//     // ==========User =============
+//     updateStatisticsUser(event.params.user,UserStatistics.amount,event.params.amount.neg(), event.address);
+//     updateStatisticsUser(event.params.user,UserStatistics.amountUnStake,event.params.amount, event.address);
+//     updateStatisticsUser(event.params.user,UserStatistics.totalUnStake,BigInt.fromI32(1), event.address);
+//     updateStatisticsUser(event.params.user,UserStatistics.totalTransaction,BigInt.fromI32(1), event.address);
+//     // ==========Dashboard =============
+//     updateStatisticsDashboard(DashBoardStatistics.totalTransaction,BigInt.fromI32(1));
+//     updateStatisticsDashboard(DashBoardStatistics.amountUnStake,event.params.amount);
+//     updateStatisticsDashboard(DashBoardStatistics.totalUnStake,BigInt.fromI32(1));
+//     if(isAddressInArray(event.address.toHexString().toLowerCase(), ContractAddress.publicPoolAddress)){
+//         // ==========User Public=============
+//         updateStatisticsUser(event.params.user,UserStatistics.amountPublic,event.params.amount.neg(), event.address);
+//         updateStatisticsUser(event.params.user,UserStatistics.amountUnStakePublic,event.params.amount, event.address);
+//         updateStatisticsUser(event.params.user,UserStatistics.totalUnStakePublic,BigInt.fromI32(1), event.address);
+//         updateStatisticsUser(event.params.user,UserStatistics.totalTransactionPublic,BigInt.fromI32(1), event.address);
+//         // ==========Dashboard Public=============
+//         updateStatisticsDashboard(DashBoardStatistics.totalTxPublic,BigInt.fromI32(1));
+//         updateStatisticsDashboard(DashBoardStatistics.amountUnStakePublic, event.params.amount)
+//         updateStatisticsDashboard(DashBoardStatistics.totalUnStakePublic,BigInt.fromI32(1));
+//     }
+//     // if(event.address.toHexString().toLowerCase() == ContractAddress.bitgetPoolAddress.toLowerCase()){
+//     if(isAddressInArray(event.address.toHexString().toLowerCase(), ContractAddress.bitgetPoolAddress)){
+//         // ==========User Bitget=============
+//         updateStatisticsUser(event.params.user,UserStatistics.amountBitget,event.params.amount.neg(), event.address);
+//         updateStatisticsUser(event.params.user,UserStatistics.amountUnStakeBitget,event.params.amount, event.address);
+//         updateStatisticsUser(event.params.user,UserStatistics.totalUnStakeBitget,BigInt.fromI32(1), event.address);
+//         updateStatisticsUser(event.params.user,UserStatistics.totalTransactionBitget,BigInt.fromI32(1), event.address);
+//         // ==========Dashboard Bitget=============
+//         updateStatisticsDashboard(DashBoardStatistics.totalTxBitget,BigInt.fromI32(1));
+//         updateStatisticsDashboard(DashBoardStatistics.amountUnStakeBitget, event.params.amount)
+//         updateStatisticsDashboard(DashBoardStatistics.totalUnStakeBitget,BigInt.fromI32(1));
+//     }
+// }
